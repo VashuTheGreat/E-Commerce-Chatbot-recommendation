3fd83289e0dfeccff1fc213e75ab32c88b0248b1
vansh Sharma <vanshsharma7832@gmail.com>
2026-03-15 19:53:08 +0530

    updated port

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ from logger import *
 
 os.makedirs("tempImage",exist_ok=True)
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
